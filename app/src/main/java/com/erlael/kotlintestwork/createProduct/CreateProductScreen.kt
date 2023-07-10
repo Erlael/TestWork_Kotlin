4741a37ccdbf7ccf30e1b7ce8c697d3baaa7ecbf
Erlael <kuran001@mail.ru>
2023-07-10 23:34:17 +0700

    Added "create product" screen

---
+++ app/src/main/java/com/erlael/kotlintestwork/createProduct/CreateProductScreen.kt
@@ -0,0 +1,103 @@
+package com.erlael.kotlintestwork.createProduct
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.erlael.kotlintestwork.data.Product
+
+
+@Composable
+fun CreateProductScreen(
+    viewModel: CreateProductViewModel,
+    navHostController: NavHostController
+) {
+    val name by viewModel.name.collectAsState()
+    val price by viewModel.price.collectAsState()
+    val count by viewModel.count.collectAsState()
+
+    val isLoading by viewModel.isLoading.collectAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            ProductField(
+                name.trim(),
+                Product::name.name
+            ) {
+                viewModel.onNameEdit(it)
+            }
+            ProductField(
+                price.toString(),
+                Product::price.name,
+                KeyboardOptions(keyboardType = KeyboardType.Number),
+            ) {
+                viewModel.onPriceEdit(it.toIntOrNull() ?: 0)
+            }
+            ProductField(
+                count.toString(),
+                Product::count.name,
+                KeyboardOptions(keyboardType = KeyboardType.Number),
+            ) {
+                viewModel.onCountEdit(it.toIntOrNull() ?: 0)
+            }
+        }
+        Button(
+            enabled = !isLoading,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            onClick = { viewModel.onAdd(navHostController) },
+        ) {
+            if (isLoading) {
+                CircularProgressIndicator(color = Color.White)
+            } else {
+                Text(text = "Add")
+            }
+        }
+    }
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun ProductField(
+    value: String,
+    label: String,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(),
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        singleLine = true,
+        modifier = Modifier
+            .fillMaxWidth(),
+        keyboardOptions = keyboardOptions,
+        label = { Text(text = label) },
+        value = value, onValueChange = onValueChange
+    )
+}
---
+++ app/src/main/java/com/erlael/kotlintestwork/createProduct/CreateProductViewModel.kt
@@ -0,0 +1,58 @@
+package com.erlael.kotlintestwork.createProduct
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavHostController
+import com.erlael.kotlintestwork.Screen
+import com.erlael.kotlintestwork.data.Product
+import com.erlael.kotlintestwork.data.ProductDao
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateProductViewModel @Inject constructor(
+    private val dao: ProductDao
+) : ViewModel() {
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    private val _name = MutableStateFlow("")
+    val name = _name.asStateFlow()
+
+    private val _price = MutableStateFlow(0)
+    val price = _price.asStateFlow()
+
+    private val _count = MutableStateFlow(0)
+    val count = _count.asStateFlow()
+
+    fun onNameEdit(name: String) { _name.value = name }
+
+    fun onPriceEdit(price: Int) { _price.value = price }
+
+    fun onCountEdit(count: Int) { _count.value = count }
+
+    fun onAdd(navHostController: NavHostController) {
+        val addJob = viewModelScope.launch {
+            _isLoading.value = true
+            delay(300L)
+            dao.insertProduct(
+                Product(
+                name = name.value,
+                price = price.value,
+                count = count.value
+            )
+            )
+        }
+
+        viewModelScope.launch {
+            addJob.join()
+            _isLoading.value = false
+            navHostController.navigate(Screen.ProductList.route)
+        }
+    }
+}
